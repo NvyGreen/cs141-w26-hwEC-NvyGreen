78dda922d32d36d8e4fad7c74ca17591ffd0f125
NvyGreen <nivedha.aravindmali@gmail.com>
2026-03-22 21:23:47 +0000

    Wrote instance variables + constructor for Disk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::sync::{Arc, RwLock};
+
+struct Disk {
+    sectors: [Arc<RwLock<String>>; Disk::NUM_SECTORS]
+}
+
+impl Disk {
+    const NUM_SECTORS: usize = 2048;
+    const DISK_DELAY: i64 = 80;
+    
+    fn new() -> Self {
+        Self {
+            sectors: std::array::from_fn(|_| {
+                Arc::new(RwLock::new(String::new()))
+            }),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("*** 141 OS Simulation ***");
 }
