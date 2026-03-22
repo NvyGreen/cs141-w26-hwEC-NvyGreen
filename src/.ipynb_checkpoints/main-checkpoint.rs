79b297290ef6030595fd91b1ada1d56768336f5c
NvyGreen <nivedha.aravindmali@gmail.com>
2026-03-22 21:42:27 +0000

    Wrote instance variables + constructor for Printer

--- src/.ipynb_checkpoints/main-checkpoint.rs
+++ src/.ipynb_checkpoints/main-checkpoint.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, RwLock};
+use std::fs::{File, OpenOptions};
 
 struct Disk {
     sectors: [Arc<RwLock<String>>; Disk::NUM_SECTORS]
@@ -16,6 +17,25 @@ impl Disk {
         }
     }
 }
+
+
+struct Printer {
+    out: File
+}
+
+impl Printer {
+    const PRINT_DELAY: i64 = 275;
+
+    fn new(id: i64) -> Self {
+        let file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("PRINTER".to_owned() + &id.to_string())
+            .unwrap();
+        Self { out: file }
+    }
+}
+
 
 fn main() {
     println!("*** 141 OS Simulation ***");
